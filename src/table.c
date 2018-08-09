4c81ad7dfd5afc054c46c85c17bb20961a23441f
Danilo Matias <danilo.maldito@gmail.com>
2018-08-09 11:29:34 -0300

    Added underline on ruby methods name.

--- src/table.c
+++ src/table.c
@@ -58,9 +58,9 @@ mrb_table_init(mrb_state* mrb)
   emv   = mrb_class_get_under(mrb, plt, "EMV");
   table = mrb_define_class_under(mrb, emv, "Table", mrb->object_class);
 
-  mrb_define_class_method(mrb , table , "init"   , mrb_emv_table_s_init   , MRB_ARGS_REQ(1));
-  mrb_define_class_method(mrb , table , "rec"    , mrb_emv_table_s_rec    , MRB_ARGS_REQ(1));
-  mrb_define_class_method(mrb , table , "fin"    , mrb_emv_table_s_fin    , MRB_ARGS_NONE());
-  mrb_define_class_method(mrb , table , "change" , mrb_emv_table_s_change , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , table , "_init"   , mrb_emv_table_s_init   , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , table , "_rec"    , mrb_emv_table_s_rec    , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , table , "_fin"    , mrb_emv_table_s_fin    , MRB_ARGS_NONE());
+  mrb_define_class_method(mrb , table , "_change" , mrb_emv_table_s_change , MRB_ARGS_REQ(1));
 }
 
